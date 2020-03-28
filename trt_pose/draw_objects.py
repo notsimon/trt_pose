b08e4aafbacea73f70d0feed21eb1f40e4184801
Simon Guillot <simon.guillot@protonmail.com>
2020-03-28 22:38:52 +0100

    Use PIL instead of OpenCV to draw pose

--- trt_pose/draw_objects.py
+++ trt_pose/draw_objects.py
@@ -1,34 +1,25 @@
-import cv2
+from PIL import ImageDraw
 
 
 class DrawObjects(object):
-    
     def __init__(self, topology):
         self.topology = topology
-        
+
     def __call__(self, image, object_counts, objects, normalized_peaks):
-        topology = self.topology
-        height = image.shape[0]
-        width = image.shape[1]
-        
-        K = topology.shape[0]
+        draw = ImageDraw.Draw(image)
+        height = image.height
+        width = image.width
+
+        K = self.topology.shape[0]
         count = int(object_counts[0])
-        K = topology.shape[0]
+        K = self.topology.shape[0]
         for i in range(count):
-            color = (0, 255, 0)
             obj = objects[0][i]
             C = obj.shape[0]
-            for j in range(C):
-                k = int(obj[j])
-                if k >= 0:
-                    peak = normalized_peaks[0][j][k]
-                    x = round(float(peak[1]) * width)
-                    y = round(float(peak[0]) * height)
-                    cv2.circle(image, (x, y), 3, color, 2)
 
             for k in range(K):
-                c_a = topology[k][2]
-                c_b = topology[k][3]
+                c_a = self.topology[k][2]
+                c_b = self.topology[k][3]
                 if obj[c_a] >= 0 and obj[c_b] >= 0:
                     peak0 = normalized_peaks[0][c_a][obj[c_a]]
                     peak1 = normalized_peaks[0][c_b][obj[c_b]]
@@ -36,4 +27,12 @@ class DrawObjects(object):
                     y0 = round(float(peak0[0]) * height)
                     x1 = round(float(peak1[1]) * width)
                     y1 = round(float(peak1[0]) * height)
-                    cv2.line(image, (x0, y0), (x1, y1), color, 2)
+                    draw.line([(x0, y0), (x1, y1)], fill=(0, 255, 0), width=2)
+
+            for j in range(C):
+                k = int(obj[j])
+                if k >= 0:
+                    peak = normalized_peaks[0][j][k]
+                    x = round(float(peak[1]) * width)
+                    y = round(float(peak[0]) * height)
+                    draw.ellipse((x - 3, y - 3, x + 3, y + 3), fill=(255, 0, 0))
